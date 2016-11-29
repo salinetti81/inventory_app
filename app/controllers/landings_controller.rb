f6956f13022270857d3478b78b9a8e01860e9f5e
Daniel Salinetti <dansalinetti@gmail.com>
2016-11-29 14:40:05 -0500

    Adding Secure User page and Devise views for styling

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,8 +1,15 @@
 class LandingsController < ApplicationController
+   before_action :authenticate_user!, only: :secure
     def index
     end
 
     def secure
+      id = current_user.id
+        user = User.find(id)
+
+        puts "============"
+        p user
+        puts "============"
     end
 
 end
