c934c2bbe4f5a91782acebe6a44238dfaaaba669
Daniel Salinetti <dansalinetti@gmail.com>
2016-11-30 12:35:25 -0500

    Adding Work Orders, Equpiment, and Employee controllers

---
+++ app/controllers/equipment_controller.rb
@@ -0,0 +1,25 @@
+class EquipmentController <ApplicationController 
+
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+end
