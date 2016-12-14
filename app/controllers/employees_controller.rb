70359542ca79caab82aceeb2c44b9e86a58e62c6
Daniel Salinetti <dansalinetti@gmail.com>
2016-12-14 14:28:22 -0500

    Setting up Employee controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -9,21 +9,52 @@ class EmployeesController <ApplicationController
     @employee = Employee.find(params[:id])
   end
 
-
-  def create
-  end
-
   def new
+    @employee = Employee.new
   end
 
   def edit
+    @employee = Employee.find(params[:id])
   end
 
+  def create
+    @employee = Employee.new(employee_params)
+
+    if @employee.employee_id
+      @employee.employee_id = @employee.employee_id
+    end
+
+    if @employee.save
+      redirect_to @employee
+    else
+      render :action => :new
+    end
+  end
  
   def update
+    @employee = Employee.find(params[:id])
+
+    if @employee.update(employee_params)
+      redirect_to @employee
+    else
+      render :action => :edit
   end
 
   def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+    redirect_to employees_path
+  end
+
+  private
+
+
+    def set_employee
+      @employee = Employee.find(params[:id])
+    end
+
+    def employee_params
+      params.require(:employee).permit(:first_name, :last_name, :employee_id)
   end
 
 
